7d835890a1c95e6de480b552aabc56040ce5d98d
Valera2312 <33982431+Valera2312@users.noreply.github.com>
2022-12-03 22:07:52 +1000

    init

---
+++ src/main/java/Main.java
@@ -0,0 +1,32 @@
+import java.util.Arrays;
+import java.util.OptionalInt;
+import java.util.stream.IntStream;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println(sumOfTwoMinimalNumbersInArray());
+    }
+    static long sumOfTwoMinimalNumbersInArray(int ...args) {
+        if(args.length == 0) return 0;
+        int min = Integer.MAX_VALUE;
+        int min2 = Integer.MAX_VALUE;
+        for (int arg : args) {
+            if (min > arg) {
+                min2 = min;
+                min = arg;
+            } else if (min2 > arg && arg != min) {
+                min2 = arg;
+            }
+        }
+        if(min2 == Integer.MAX_VALUE) {
+            min2 = 0;
+        }
+        return min2 + min;
+    }
+    static long sumOfTwoMinimalNumbersInArrayStream(int ...args) {
+
+         OptionalInt sumOfTwoNumbers = Arrays.stream(args).sorted().limit(2).distinct().reduce(Integer::sum);
+         return sumOfTwoNumbers.orElse(0);
+
+    }
+}
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,72 @@
+import jdk.jfr.Label;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class MainTest {
+    static int[] emptyArray = {};
+    static int[] arrayWithTheSameElements = new int[10];
+    static int[] arrayWithMillionElements = new int[1000000];
+    static long start;
+    @BeforeEach
+    void initTime() {
+        start = System.currentTimeMillis();
+    }
+
+
+    @BeforeAll
+    static void init() {
+
+        for (int i = 0; i < 10; i++) {
+            arrayWithTheSameElements[i] = 1;
+        }
+        for (int i = 0; i < 1000000; i++) {
+            arrayWithMillionElements[i] = i;
+        }
+    }
+
+    @Test
+    void sumOfTwoMinimalNumbersInArrayMillionElements() {
+
+       Assertions.assertEquals( 1,Main.sumOfTwoMinimalNumbersInArray(arrayWithMillionElements));
+        System.out.print("sumOfTwoMinimalNumbersInArrayMillionElements ");
+        System.out.println((double) (System.currentTimeMillis() - start));
+    }
+    @Test
+    void sumOfTwoMinimalNumbersInArraysTheSameElements() {
+
+        Assertions.assertEquals( 1,Main.sumOfTwoMinimalNumbersInArray(arrayWithTheSameElements));
+        System.out.print("sumOfTwoMinimalNumbersInArraysTheSameElements ");
+        System.out.println((double) (System.currentTimeMillis() - start));
+    }
+    @Test
+    void sumOfTwoMinimalNumbersInArraysEmptyArray() {
+
+        Assertions.assertEquals( 0,Main.sumOfTwoMinimalNumbersInArray(emptyArray));
+        System.out.print("sumOfTwoMinimalNumbersInArraysEmptyArray ");
+        System.out.println((double) (System.currentTimeMillis() - start));
+    }
+    @Test
+    void sumOfTwoMinimalNumbersInArrayStreamMillionElements() {
+        long m = System.currentTimeMillis();
+        Assertions.assertEquals( 1,Main.sumOfTwoMinimalNumbersInArrayStream(arrayWithMillionElements));
+        System.out.print("sumOfTwoMinimalNumbersInArrayStreamMillionElements ");
+        System.out.println((double) (System.currentTimeMillis() - m));
+    }
+    @Test
+    void sumOfTwoMinimalNumbersInArrayTheSameElementsStream() {
+        long m = System.currentTimeMillis();
+        Assertions.assertEquals( 1,Main.sumOfTwoMinimalNumbersInArrayStream(arrayWithTheSameElements));
+        System.out.print("sumOfTwoMinimalNumbersInArrayTheSameElementsStream ");
+        System.out.println((double) (System.currentTimeMillis() - m));
+    }
+    @Test
+    void sumOfTwoMinimalNumbersInArraysEmptyArrayStream() {
+
+        Assertions.assertEquals( 0,Main.sumOfTwoMinimalNumbersInArrayStream(emptyArray));
+        System.out.print("sumOfTwoMinimalNumbersInArraysEmptyArrayStream ");
+        System.out.println((double) (System.currentTimeMillis() - start));
+    }
+
+}
